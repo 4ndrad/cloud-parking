876f84eea50846dbc76ad8bce23fdcbcc4deca67
Natan Andrade de Lima <natanal@primeit.com.br>
2022-08-02 15:32:47 -0300

    add o Swagger

--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.parking.service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,8 +18,11 @@ public class ParkingService {
 	
 	static {
 		var id = getUUID();
+		var id1 = getUUID();
 		Parking parking = new Parking(id, "DMS-1111", "SC", "CELTA", "PRETO", null, null, null);
+		Parking parking2 = new Parking(id1, "FGT-4895", "SP", "CORSA", "VERDE", null, null, null);
 		parkingMap.put(id, parking);
+		parkingMap.put(id1, parking2);
 	}
 	
 	public List<Parking> findAll(){
@@ -27,6 +31,18 @@ public class ParkingService {
 
 	private static String getUUID() {
 		return UUID.randomUUID().toString().replace("-", "");
+	}
+
+	public Parking findById(String id) {
+		return parkingMap.get(id);
+	}
+
+	public Parking create(Parking parkingCreate) {
+		String uuid = getUUID();
+		parkingCreate.setId(uuid);
+		parkingCreate.setEntryDate(LocalDateTime.now());
+		parkingMap.put(uuid, parkingCreate);
+		return parkingCreate;
 	}
 	
 }
